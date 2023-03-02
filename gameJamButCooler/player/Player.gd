207151b30777a21428d545a4b0d1f40371c6fa3b
Harry282 <iamharry282@gmail.com>
2023-03-02 12:03:48 -0500

    Player hit event

--- gameJamButCooler/player/Player.gd
+++ gameJamButCooler/player/Player.gd
@@ -1,20 +1,18 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal hit
 
 export var speed = 400
 var screen_size
 
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
     screen_size = get_viewport_rect().size
-    pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     var velocity = Vector2.ZERO
     if Input.is_action_pressed("move_right"):
@@ -43,4 +41,8 @@ func _process(delta):
     position += velocity * delta
     position.x = clamp(position.x, 0, screen_size.x)
     position.y = clamp(position.y, 0, screen_size.y)
-    pass
+
+func _on_Player_body_entered(body):
+    hide()
+    emit_signal("hit")
+    $CollisionShape2D.set_deferred("disabled", true)
