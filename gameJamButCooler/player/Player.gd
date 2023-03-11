7b36fd52f98245054eb59ebc46d966330ef3bfb4
JimmyJTG <Acwlark2005@gmail.com>
2023-03-11 03:42:19 -0500

    Created, implemented player walk animation

--- gameJamButCooler/player/Player.gd
+++ gameJamButCooler/player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 690
-const ACCELERATION = 1000
+const MAX_SPEED = 450
+const ACCELERATION = 600
 const FRICTION = 2000
 const JUMP_SPEED = 555
 
@@ -13,16 +13,19 @@ func _ready():
 
 func _physics_process(delta):
     var move = ACCELERATION * (Input.get_axis("move_left", "move_right"))
-    
+    $AnimatedSprite.animation = "walk"
     if abs(move) < ACCELERATION * 0.1:
         velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+        $AnimatedSprite.set_frame(0) 
+        $AnimatedSprite.stop()
     else:
+        $AnimatedSprite.flip_h = velocity.x < 0
         var acceleration = move * delta
         if velocity.x * move < 0:
             acceleration *= 5
         acceleration = clamp(acceleration, -MAX_SPEED - velocity.x, MAX_SPEED - velocity.x)        
         velocity.x += acceleration
-    
+        $AnimatedSprite.play()
     velocity.y += gravity * delta
     
     velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
