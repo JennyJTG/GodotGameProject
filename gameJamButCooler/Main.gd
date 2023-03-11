2c6d724b2fbf53d1bb267d114909d0a114254ea8
Para-ethyltoluene <126699082+Para-ethyltoluene@users.noreply.github.com>
2023-03-11 16:53:10 -0500

    Hello I added enemy lmao

--- gameJamButCooler/Main.gd
+++ gameJamButCooler/Main.gd
@@ -1,10 +1,10 @@
 extends Node
 
 func _ready():
-    randomize()
+	randomize()
 
 func _process(delta):
-    if $Player.velocity != null:
-        var x = $Player.velocity.x
-        var y = $Player.velocity.y
-        $HUD.show_message(" Velocity:\n {x}\n {y}".format({"x" : x, "y" : y}))
+	if $Player.velocity != null:
+		var x = $Player.velocity.x
+		var y = $Player.velocity.y
+		$HUD.show_message(" Velocity:\n {x}\n {y}".format({"x" : x, "y" : y}))
