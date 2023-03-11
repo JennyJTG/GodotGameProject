369b48398e09274385ad3c0e7bac0351cf62505d
JimmyJTG <Acwlark2005@gmail.com>
2023-03-11 13:26:03 -0500

    added falling animation + bug fixes
    bugs:
    tilemap not snapping properly
    glitchy animation during turning

--- gameJamButCooler/player/Player.gd
+++ gameJamButCooler/player/Player.gd
@@ -13,19 +13,24 @@ func _ready():
 
 func _physics_process(delta):
     var move = ACCELERATION * (Input.get_axis("move_left", "move_right"))
-    $AnimatedSprite.animation = "walk"
-    if abs(move) < ACCELERATION * 0.1:
-        velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+    if velocity.y > 0 and !is_on_floor():
+        $AnimatedSprite.animation = "falling"
+    else:
+        $AnimatedSprite.animation = "walk"
+    if velocity.length() > 0:
+        $AnimatedSprite.flip_h = velocity.x < 0
+        $AnimatedSprite.play()
+    else:
         $AnimatedSprite.set_frame(0) 
         $AnimatedSprite.stop()
+    if abs(move) < ACCELERATION * 0.1:
+        velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
     else:
-        $AnimatedSprite.flip_h = velocity.x < 0
         var acceleration = move * delta
         if velocity.x * move < 0:
             acceleration *= 5
         acceleration = clamp(acceleration, -MAX_SPEED - velocity.x, MAX_SPEED - velocity.x)        
         velocity.x += acceleration
-        $AnimatedSprite.play()
     velocity.y += gravity * delta
     
     velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
