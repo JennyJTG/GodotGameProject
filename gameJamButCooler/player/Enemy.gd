3abc2acb4e616f3c433493aab618ff99acfdce3f
Para-ethyltoluene <126699082+Para-ethyltoluene@users.noreply.github.com>
2023-03-12 14:29:48 -0400

    Ok i fixed spaces thingy are yall happy now? Bruh
    Yes

--- gameJamButCooler/player/Enemy.gd
+++ gameJamButCooler/player/Enemy.gd
@@ -8,26 +8,28 @@ var velocity = Vector2()
 var HP = 25
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _ready():
-	pass
 
+
+func _ready():
+    $HPLabel.text = str(HP)
+    pass
 
 func dep_health(amt):
-	HP = HP - amt
-	$HPLabel.text = str(HP)
-	
-	
+    HP = HP - amt
+    $HPLabel.text = str(HP)
+    
+    
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
+    velocity.y += gravity * delta
+    velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 
 
 
 func _on_Area2D_body_entered(body):
-	var hit = body
-	if hit.get_name() == "Player":
-		if hit.HP > 0:
-			hit.dep_health(HP)
-			dep_health(HP)
-	pass
+    var hit = body
+    if hit.get_name() == "Player":
+        if hit.HP > 0:
+            hit.dep_health(HP)
+            dep_health(HP)
+    pass
