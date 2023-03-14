fea4c83210fcd6809aff281d168113405fc00036
JimmyJTG <Acwlark2005@gmail.com>
2023-03-14 04:46:27 -0400

    Updated enemy + bug fixes
    added wizard assets + coded movement
    
    bug fixes
    - fixed movement glitch in floor
    - added invisible wall
    
    TODO
    - comments
    - naming and refactoring

--- gameJamButCooler/player/Enemy.gd
+++ gameJamButCooler/player/Enemy.gd
@@ -2,15 +2,16 @@ extends "res://Entity.gd"
 
 
 
-##Among us
-##Haha whoever wrote play I stole all yer scripts
 var velocity = Vector2()
+var pastPosition = position
+var faceRight = -1
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 
 func _ready():
     HP = 25
+    velocity.x = 100
     $HPLabel.text = str(HP)
     pass
 
@@ -19,9 +20,18 @@ func _ready():
 func _physics_process(delta):
     velocity.y += gravity * delta
     velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
-
-
-
+    $AnimatedSprite.animation = "walk"
+    if velocity.length() > 0:
+        $AnimatedSprite.flip_h = velocity.x < 0
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.set_frame(0) 
+        $AnimatedSprite.stop()
+    if position == pastPosition:
+        velocity.x = 100*faceRight
+        faceRight *= -1
+    pastPosition = position
+    
 func _on_Area2D_body_entered(body):
     var hit = body
     if hit.get_name() == "Player":
@@ -29,3 +39,4 @@ func _on_Area2D_body_entered(body):
             hit.dep_health(HP)
             dep_health(HP)
     pass
+
